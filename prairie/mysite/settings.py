b7bdeb59637151734f05dad5dfbe3fde82a7221e
Anthony Mandelli <mandelliant@gmail.com>
2023-01-18 11:24:54 -0500

    fixed secret key leak
    secret key now set by environment variable

--- prairie/mysite/settings.py
+++ prairie/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-floyac&go2n7168i$bq3&(-b5oix+09@qlp)54sor1-l7!#8q$"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
